c724f24f465b74511affbf37e980ff04000ddb32
artemdiduk <68499262+artemdiduk@users.noreply.github.com>
2021-09-29 22:23:09 +0300

    Add files via upload
    deploy

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+$(function () {
+    
+$('.header__list-item.dropdown').click(function() {
+  $(this).toggleClass('active');
+})
+$('.information__slider').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 700,
+
+});
+$('.reviews__inner').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: true,
+  dots: true,
+  prevArrow: '<button type="button" class="reviews__arrow left"><img src="../images/arrow-left.svg" alt="" class="reviews__arrow-img"></button>',
+  nextArrow: '<button type="button" class="reviews__arrow right"><img src="../images/arrow-right.svg" alt="" class="reviews__arrow-img"></button> ',
+  responsive: [
+    {
+      breakpoint: 1190,
+      settings: {
+       
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    
+  ]
+
+});
+
+
+  
+$('.header__menu-burger').click(function() {
+  $(this).toggleClass('active');
+  $('.header__menu-list').toggleClass('active');
+})
+
+})
